366377a3119371f7e47b5942b6fc9a1ec1479ab8
Pavel <triodjangopiter@yandex.ru>
2018-04-18 22:16:47 +0300

    add basic create read update operation for vds acc phone

--- src/main/java/ru/pravvich/repository/SocialAccountRepository.java
+++ src/main/java/ru/pravvich/repository/SocialAccountRepository.java
@@ -1,13 +1,8 @@
 package ru.pravvich.repository;
 
-import lombok.NonNull;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import ru.pravvich.domain.SocialAccount;
-
-import java.util.Collection;
-import java.util.List;
 
 @Repository
 public interface SocialAccountRepository extends CrudRepository<SocialAccount, Integer> {
