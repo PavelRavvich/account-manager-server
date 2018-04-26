60b84bd2e70cf1fc2024b516c0f6e883b097a23e
Pavel <triodjangopiter@yandex.ru>
2018-04-27 01:25:59 +0300

    complete Phone controller tests

--- src/test/java/ru/pravvich/web/phone/PhoneControllerTest.java
+++ src/test/java/ru/pravvich/web/phone/PhoneControllerTest.java
@@ -21,6 +21,7 @@ import java.sql.Timestamp;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
 
 @SpringBootTest
 @WebAppConfiguration
@@ -42,29 +43,60 @@ public class PhoneControllerTest {
         phone = new Phone();
         phone.setAccounts(Lists.newArrayList());
         phone.setRegDate(new Timestamp(System.currentTimeMillis()));
-        phone.setOperatorAccPassword("test_password");
-        phone.setOperatorUrl("test_operator_url");
-        phone.setOperatorAccLogin("test_login");
-        phone.setOperatorName("test_op_name");
-        phone.setNumber("test_phone_number");
-        phone.setStatus("test_status");
-        phone.setNote("teat_note");
         phone.setId(1);
         filter = new PhoneFilter(new PageRequest(0, 1));
     }
 
-
     @Test
-    public void whenCallGetThenGotPhone() {
+    public void whenCallGetThenThenGotPhone() {
         given(phoneService.get(1)).willReturn(phone);
         PhoneRest result = phoneController.get(1);
         assertThat(result.getId(), is(1));
     }
 
     @Test
-    public void whenCallListReturnData() {
+    public void whenCallListWithRegDateNullThenReturnData() {
         given(phoneService.list(filter)).willReturn(new PageImpl<>(Lists.newArrayList(phone)));
         RestList result = phoneController.list(1, 0, null, null, null, null, null, null, null, null, null);
         assertThat(result.getData().size(), is(1));
+    }
+
+    @Test
+    public void whenCallListWithRegDateNonNullThenReturnData() {
+        long date = System.currentTimeMillis();
+        filter.setFrom(new Timestamp(date));
+        filter.setTo(new Timestamp(date));
+        given(phoneService.list(filter)).willReturn(new PageImpl<>(Lists.newArrayList(phone)));
+        RestList result = phoneController.list(1, 0, null, null, null, null, null, null, null, date, date);
+        assertThat(result.getData().size(), is(1));
+    }
+
+    @Test
+    public void whenSavePhoneThenReturnSamePhone() {
+        Phone phone = new Phone();
+        phone.setId(1);
+        given(phoneService.saveOrUpdate(phone)).willReturn(phone);
+        PhoneRest phoneRest = new PhoneRest();
+        phoneRest.setId(1);
+        PhoneRest result = phoneController.save(phoneRest);
+        assertThat(result.getId(), is(1));
+    }
+
+    @Test
+    public void whenUpdatePhoneThenReturnSamePhone() {
+        Phone phone = new Phone();
+        phone.setId(1);
+        given(phoneService.saveOrUpdate(phone)).willReturn(phone);
+        PhoneRest phoneRest = new PhoneRest();
+        phoneRest.setSocialAccIds(Lists.newArrayList(1));
+        phoneRest.setId(1);
+        PhoneRest result = phoneController.update(phoneRest);
+        assertThat(result.getId(), is(1));
+    }
+
+    @Test
+    public void whenDelete() {
+        phoneController.delete(1);
+        verify(phoneService).delete(1);
     }
 }
