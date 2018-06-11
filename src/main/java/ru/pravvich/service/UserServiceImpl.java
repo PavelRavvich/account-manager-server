ddb5e158756b563b2327a2ea3fd02f3b24fb3968
Pavel <triodjangopiter@yandex.ru>
2018-06-11 06:13:30 +0300

    add docs for VdsService and UserService

--- src/main/java/ru/pravvich/service/UserServiceImpl.java
+++ src/main/java/ru/pravvich/service/UserServiceImpl.java
@@ -8,17 +8,33 @@ import ru.pravvich.repository.UserRepository;
 
 /**
  * @author Pavel Ravvich.
+ *
+ * Service for User entity.
  */
 @Service
 public class UserServiceImpl implements UserService {
-
+    /**
+     * Repository layer.
+     */
     private final UserRepository userRepository;
 
+    /**
+     * Default constructor.
+     *
+     * @param userRepository user repository layer.
+     */
     @Autowired
     public UserServiceImpl(@NonNull UserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
+    /**
+     * Get User by username.
+     * Auth method for UserDetailServiceImpl.
+     *
+     * @param username of auth user.
+     * @return User if exist or null if user does't exist.
+     */
     @Override
     public User getUserByUsername(String username) {
         return userRepository.findByUsername(username);
--- src/main/java/ru/pravvich/service/VdsServiceImpl.java
+++ src/main/java/ru/pravvich/service/VdsServiceImpl.java
@@ -15,41 +15,76 @@ import static org.springframework.data.jpa.domain.Specifications.where;
 
 /**
  * @author Pavel Ravvich.
+ *
+ * Service layer for Vds entity.
  */
 @Service
 public class VdsServiceImpl implements VdsService {
-
+    /**
+     * Repository for Vds.
+     */
     private final VdsRepository vdsRepository;
-
+    /**
+     * Repository for SocialAccounts.
+     */
     private final SocialAccountRepository socialAccountRepository;
 
+    /**
+     * Default constructor.
+     * Injection repository layer.
+     *
+     * @param vdsRepository repo Vds.
+     * @param socialAccountRepository repo SocialAccount.
+     */
     @Autowired
     public VdsServiceImpl(@NonNull VdsRepository vdsRepository, @NonNull SocialAccountRepository socialAccountRepository) {
         this.vdsRepository = vdsRepository;
         this.socialAccountRepository = socialAccountRepository;
     }
 
+    /**
+     * Get Vds list by filter.
+     *
+     * @param filter from UI for select by criteria.
+     * @return list of Vds corresponding filter state.
+     */
     @Override
     public Page<Vds> list(@NonNull final VdsFilter filter) {
         VdsSpecification specs = new VdsSpecification(filter);
         return vdsRepository.findAll(where(specs), filter.getPageable());
     }
 
+    /**
+     * Get Vds by id.
+     *
+     * @param id for select.
+     * @return Vds with equal id or empty Vds obj.
+     */
     @Override
     public Vds get(final int id) {
         Vds vds = vdsRepository.findOne(id);
         return nonNull(vds) ? vds : new Vds();
     }
 
+    /**
+     * Save new Vds or update if vds.getId() already exist.
+     *
+     * @param vds actual state for save to db.
+     * @return created or updated Vds.
+     */
     @Override
     public Vds saveOrUpdate(@NonNull final Vds vds) {
         return vdsRepository.save(vds);
     }
 
+    /**
+     * Delete Vds by id.
+     *
+     * @param id of Vds for delete.
+     */
     @Override
     public void delete(final int id) {
         socialAccountRepository.setVds(null, id);
         vdsRepository.delete(id);
     }
-
 }
