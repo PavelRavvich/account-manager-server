0e3209330d7a2b51301dd170457ca8034e5c5ecc
Pavel <triodjangopiter@yandex.ru>
2018-06-20 10:02:01 +0300

    add docs for PhoneRest

--- src/main/java/ru/pravvich/web/phone/PhoneRest.java
+++ src/main/java/ru/pravvich/web/phone/PhoneRest.java
@@ -12,40 +12,62 @@ import java.util.List;
 
 /**
  * @author Pavel Ravvich.
+ *
+ * Rest representation Phone obj.
  */
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
 class PhoneRest {
-
+    /**
+     * Phone id.
+     */
     @JsonProperty("id")
     private int id;
-
+    /**
+     * Active status of phone.
+     */
     @JsonProperty("status")
     private String isActive;
-
+    /**
+     * Phone number.
+     */
     @JsonProperty("num")
     private String number;
-
+    /**
+     * Account login on GSM operator website.
+     */
     @JsonProperty("opLogin")
     private String opLogin;
-
+    /**
+     * Account password on GSM operator website.
+     */
     @JsonProperty("opPassword")
     private String opPassword;
-
+    /**
+     * Name of GSM operator.
+     */
     @JsonProperty("opName")
     private String opType;
-
+    /**
+     * URL link to GSM operator website.
+     */
     @JsonProperty("opUrl")
     private String opUrl;
-
+    /**
+     * Any note about current phone.
+     */
     @JsonProperty("note")
     private String note;
-
+    /**
+     * SocialAccounts which registered with this phone number.
+     */
     @JsonProperty("socialAccIds")
     private List<Integer> socialAccIds;
-
+    /**
+     * Registration date.
+     */
     @JsonProperty("regDate")
     @JsonFormat(shape = JsonFormat.Shape.NUMBER, pattern = "s")
     private Timestamp regDate;
